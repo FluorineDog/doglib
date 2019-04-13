976f1400817439cf13e17c5268db5dd5f9d36af0
fluorinedog <fluorinedog@gmail.com>
2019-04-14 01:03:25 +0800

    tmp

--- doglib/common/range.h
+++ doglib/common/range.h
@@ -1,26 +1,31 @@
 #pragma once
 namespace doglib {
 namespace common {
-
-
 namespace impl {
 template <class T>
 class RangeImpl {
   public:
     RangeImpl(T beg, T end) : beg(beg), end(end) {}
     DataIter<T> begin() {
-        return
+        return DataIter<T>(beg);
     }
-
+    DataIter<T> end() {
+        return DataIter<T>(end);
+    }
   private:
     T beg, end;
-}
+};
 
 }    // namespace impl
 
 template <typename T>
 inline auto Range(T beg, T end) -> impl::RangeImpl<T> {
-    return
+    return RangeImpl(beg, end);
+}
+
+template <typename T>
+inline auto Range(T end) -> impl::RangeImpl<T> {
+    return RangeImpl(T(0), end);
 }
 
 }    // namespace std
