7e5fcfb0fe785c08ae0cb42cda7d72f390cd6653
fluorinedog <fluorinedog@gmail.com>
2019-05-29 09:26:22 +0800

    update

---
+++ testing/graph/test_basic.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include "doglib/common/common.h"
+#include "doglib/common/utils.h"
+#include "doglib/graph/geometry.h"
+using namespace doglib;
+using namespace doglib::common;
+
+TEST(GraphBasic, naive) {
+    EXPECT_EQ(1, 1);
+}
